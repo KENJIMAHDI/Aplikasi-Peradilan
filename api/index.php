63f8f75d5702141c2862f85d528fd83b6707d51f
KENJI MAHDI <kenji23trjt@mahasiswa.pcr.ac.id>
2026-08-29 20:18:52 +0700

    fix index php standard kernel

--- api/index.php
+++ api/index.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// 1. Buat folder temporary di Vercel agar tidak permission error
+// Buat folder temporary di Vercel agar tidak permission error
 $dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
@@ -18,13 +18,20 @@ foreach ($dirs as $dir) {
     }
 }
 
-// 2. Load Autoloader & Application (Format Laravel 11)
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
-// 3. Arahkan storage path ke temporary folder
+// Pindahkan storage ke /tmp
 $app->useStoragePath('/tmp/storage');
 
-// 4. Jalankan Aplikasi (Laravel 11 Way)
-$app->handleRequest(Request::capture());
+// Proses HTTP Request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
